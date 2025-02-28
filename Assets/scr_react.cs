71e907a0f4d68745bf5911f7cf77795fe02314fe
OtoSh8 <2201105@student.toa.edu.my>
2025-03-01 04:20:15 +0800

    shifted buyaodong2 road path curve + added ghost nearby bpm + screenshake effects

--- Assets/scr_react.cs
+++ Assets/scr_react.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -7,6 +8,8 @@ public class scr_react : MonoBehaviour
 {
     public int threshold = 50;
     private bool isNearby = false;
+
+    private bool Triggered = false;
 
     [SerializeField] scr_bpm bpm;
     [SerializeField] Volume vol;
@@ -21,13 +24,17 @@ public class scr_react : MonoBehaviour
         if (IsNear() && isNearby == false)
         {
             //SPIRIT IS NEARBY
-            Anxious();
+            if(Triggered == false)
+            {
+                StartCoroutine(Anxious());
+                Triggered = true;
+            }
             isNearby = true;
         }
-        else if(isNearby == true)
+        else if (isNearby == true)
         {
             //SPIRIT NOT NEAR ANYMORE
-            CalmDown();
+            Triggered = false;
         }
     }
 
@@ -47,12 +54,18 @@ public class scr_react : MonoBehaviour
     }
 
 
-    private void Anxious()
+    IEnumerator Anxious()
     {
         // SET BPM, START CAMERA GLITCH, START OVERLAY ANIMATION
         bpm.bpm = 90;
         
         anavol.scanLineJitter.Override(0.5f);
+        anavol.colorDrift.Override(0.3f);
+
+        yield return new WaitForSeconds(5f);
+
+        CalmDown();
+        yield return null;
 
     }
 
@@ -60,6 +73,7 @@ public class scr_react : MonoBehaviour
     {
         bpm.bpm = 60;
         anavol.scanLineJitter.Override(0f);
+        anavol.colorDrift.Override(0f);
 
     }
 }
